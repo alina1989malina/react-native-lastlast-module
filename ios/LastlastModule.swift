b2c73c2875f739fdd38ee89ff852101b10adc3cf
Alina Alinovna <alinaalinovna@Mac-mini-Alina.local>
2020-08-30 23:18:36 +0300

    add

--- ios/LastlastModule.swift
+++ ios/LastlastModule.swift
@@ -1,8 +1,31 @@
-@objc(LastlastModule)
-class LastlastModule: NSObject {
+import CoreNFC
 
+@available(iOS 13.0, *)
+@objc(LastlastModule)
+class LastlastModule: NSObject , NFCTagReaderSessionDelegate {
+    
+    var session:NFCTagReaderSession?
+    
     @objc(multiply:withB:withResolver:withRejecter:)
     func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-        resolve(a*b)
+        //resolve(a*b)
+        
+        session = NFCTagReaderSession(pollingOption: .iso14443, delegate: self, queue: DispatchQueue.global())
+        session?.alertMessage = "Hold your iPhone near the ISO7816 tag to begin transaction."
+        session?.begin()
     }
+    
+    func tagReaderSession(_ session: NFCTagReaderSession, didDetect tags: [NFCTag]) {
+        print("hey there")
+        print(tags.count)
+    }
+    
+    func tagReaderSessionDidBecomeActive(_ session: NFCTagReaderSession) {
+        print("active")
+    }
+    
+    func tagReaderSession(_ session: NFCTagReaderSession, didInvalidateWithError error: Error) {
+        print("error")
+    }
+    
 }
